b6c4df1b6d1a3908a70a7c45192b8a3d0f35531a
Yousef Na <yousefnasr0541@gmail.com>
2024-10-13 18:33:57 +0300

    implemnt binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,1 +1,21 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let first = 0;
+    let last = haystack.length;
+
+    if (haystack[first] === needle) return true;
+    if (haystack.length === 1) return false;
+    if (haystack[last] === needle) return true;
+
+    while (last !== first) {
+        const mid = Math.floor((last + first) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        } else if (haystack[mid] > needle) {
+            last = mid;
+        } else {
+            first = mid + 1;
+        }
+    }
+
+    return false;
+}
