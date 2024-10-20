b6dfee02af51656492e5e3597db8f0913e14e789
Yousef Na <yousefnasr0541@gmail.com>
2024-10-20 16:43:36 +0300

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,38 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+  prev?: Node<T>;
+};
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+  }
 
-    
-
-    constructor() {
+  push(item: T): void {
+    const node: Node<T> = { value: item };
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      return;
     }
+    this.head.next = node;
+    node.prev = this.head;
+    this.head = node;
+  }
 
-    push(item: T): void {
+  pop(): T | undefined {
+    if (!this.head) return undefined;
+    this.length--;
+    const head = this.head;
+    this.head = head.prev;
+    head.prev = undefined;
+    return head.value;
+  }
 
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
